e6152397d87a4932b57a3bda2fe671fe5aef6250
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-08-23 11:35:31 +0300

    commit tree a226f74946140357a8d52f63f1d5b3dd2677891b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES ("Mr. Chocolate", 10, "M", "Blue", "High", TRUE); 
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES ("Rowdy", 15, "F", "Red", "Low", FALSE); 
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES ("Tabitha", 17, "M", "Green", "High", TRUE); 
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES ("Sergeant Brown", 20, "F", "Blue", "Low", FALSE); 
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES ("Melissa", 12, "F", "Black", "High", TRUE); 
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES ("Grinch", 16, "M", "Blue", "Low", TRUE); 
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES ("Wendy", 14, "M", "Yellow", "High", FALSE); 
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES (NULL, 12, "M", "Blue", "High", FALSE); 
